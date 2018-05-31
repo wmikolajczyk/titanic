6e889a22bab78cb37f59468ee95fd00531df9cf7
Wojtek Mikołajczyk <mikolajczyk.wojtas@gmail.com>
2018-05-31 09:39:55 +0200

    Get submission file with predictions

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score
 
 # https://www.dataquest.io/blog/kaggle-fundamentals/
 test = pd.read_csv("test.csv")
@@ -39,4 +40,40 @@ columns = ['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'Sex_male',
        'Age_categories_Child', 'Age_categories_Teenager',
        'Age_categories_Young Adult', 'Age_categories_Adult',
        'Age_categories_Senior']
-lr.fit(train[columns], train['Survived'])
+
+
+holdout = test
+
+all_X = train[columns]
+all_y = train['Survived']
+
+# train_X, test_X, train_y, test_y = train_test_split(all_X, all_y, test_size=0.2, random_state=0)
+
+# lr.fit(train_X, train_y)
+
+# predictions = lr.predict(test_X)
+# accuracy = accuracy_score(test_y, predictions)
+# print(accuracy)
+# 0.81
+
+lr = LogisticRegression()
+scores = cross_val_score(lr, all_X, all_y, cv=10)
+scores.sort()
+accuracy = scores.mean()
+print(scores)
+print(accuracy)
+# 0.80
+
+# Final model
+
+lr = LogisticRegression()
+lr.fit(all_X, all_y)
+holdout_predictions = lr.predict(holdout[columns])
+
+holdout_ids = holdout['PassengerId']
+submission_df = {
+    'PassengerId': holdout_ids,
+    'Survived': holdout_predictions
+}
+submission = pd.DataFrame(submission_df)
+submission.to_csv('submission.csv', index=False)
